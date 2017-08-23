b740d2694c107c0c82c23c16a8fa52f360500326
Danny Neira <danny.neira@gmail.com>
2017-08-23 19:48:13 -0400

    imported templates/*.html and helpers.py

---
+++ helpers.py
@@ -0,0 +1,82 @@
+from flask import Markup
+from re import match
+from hashlib import sha256
+from random import choice
+import string
+
+def gen_hash(password, seed=None):
+    if not seed:
+        seed = "".join(choice(string.ascii_letters) for i in range(5))
+    return "{0},{1}".format(sha256(str.encode(password+seed)).hexdigest(),seed)
+
+def check_hash(hash, password):
+    hash,seed = hash.split(',')
+    return True if hash[0] == sha256(str.encode(password+hash[1])).hexdigest() else False
+
+def is_empty(input):
+    return True if input == "" else False
+
+def has_space(input):
+    return True if " " in input else False
+
+def valid_len(value):
+    val_len = len(value)
+    return True if val_len >= 3 and val_len <= 20 else False
+
+def valid_pass(password, verify):
+    return True if password == verify else False
+
+def valid_email(email):
+    reg = match(r"[^@]+@[^@]+\.[^@]+", email)
+    return True if reg != None else False
+
+def validate_signup(email, password, verify):
+    errors = {'email':"",'email_err':"",'password_err':"",'verify_err':""}
+    
+    # Validate Emails
+    if not is_empty(email):
+        if has_space(email):
+            errors['email_err'] = "Your email cannot contain spaces"
+        elif not valid_len(email):
+            errors['email_err'] = "That's not a valid email length (3-20 characters)"
+        elif not valid_email(email):
+            errors['email_err'] = "That's not a valid email (hello@gmail.com)"
+
+    # Validate Passwords
+    if is_empty(password) or is_empty(verify):
+        errors['password_err'] = "You must enter a password"
+        errors['verify_err'] = "You must enter a verification password"
+    elif has_space(password):
+        errors['password_err'] = "Your password cannot contain spaces"
+    elif not valid_len(password):
+        errors['password_err'] = "That's not a valid password length (3-20 characters)"
+    elif not valid_pass(password, verify):
+        errors['password_err'] = "Passwords don't match"
+
+    # Check for errors
+    if any(True if val != "" else False for val in errors.values()):
+        if errors['email_err'] == "":
+            errors['email'] = email
+        return errors
+    else:
+        return True
+
+def validate_login(user, password):
+    errors = {'email':"",'email_err':"",'password_err':""}
+
+    # Validate Emails
+    if user == None:
+        errors['email_err'] = "Email not found, please try again or Register"
+    # Validate Passwords
+    if user and password == "":
+        errors['email'] = user.email
+        errors['password_err'] = "Please enter your password"
+    elif user and not check_pw_hash(password, user.password):
+        errors['email'] = user.email
+        errors['password_err'] = "Incorrect password, please try again"
+    
+    # Return True is there are no Errors
+    if any(True if val != "" else False for val in errors.values()):
+        return errors
+    else:
+        return True
--- main.py
+++ main.py
@@ -1,35 +1,105 @@
 from flask import Flask, request, redirect, session, render_template, flash, make_response
 from flask_sqlalchemy import SQLAlchemy
-from hashlib import sha256
-from random import choice
-import string
+from helpers import validate_signup, validate_login
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:root@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'f72746fd810a750dbee37dc116c2aa6aaf070df82d0bd7edb42bfbb42c96e9b3'
 
-def gen_hash(password, seed=None):
-    if not seed:
-        seed = "".join(choice(string.ascii_letters) for i in range(5))
-    return "{0},{1}".format(sha256(str.encode(password+seed)).hexdigest(),seed)
-
-def check_hash(hash, password):
-    hash,seed = hash.split(',')
-    return True if hash[0] == sha256(str.encode(password+hash[1])).hexdigest() else False
-
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
+    base_title = db.Column(db.String(120))
     body = db.Column(db.String(480))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id')
 
-    def __init__(self, title, body):
-        self.title = title
+    def __init__(self, base_title, body, owner):
+        self.base_title = base_title
         self.body = body
+        self.owner = owner
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def register():
+    if request.method== 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # validate user data
+        existing_user = User.query.filter_by(email=email).first()
+        validation = validate_signup(email,password,verify)
+
+        if validation == True and not existing_user:
+            new_user = User(email, make_pw_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['email'] = email
+            flash("Registered as " + email, 'info')
+            return redirect('/')
+        else:
+            flash("Registration Failed!", 'error')
+            return render_template('signup.html',
+                base_title='Register',
+                email=validation['email'],
+                email_err=validation['email_err'],
+                password_err=validation['password_err'],
+                verify_err=validation['verify_err'])
+    else:        
+        return render_template('signup.html', base_title='Register')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        validation = validate_login(user, password)
+
+        if validation == True:
+            session['email'] = email
+            flash("Welcome "+email, 'info')
+            return redirect('/')
+        else:
+            flash("Login Failed!", 'error')
+            return render_template('login.html',
+                base_title='Login Error!',
+                email=validation['email'],
+                email_err=validation['email_err'],
+                password_err=validation['password_err'])
+    else:
+        return render_template('login.html', base_title='Login')
+
+@app.route('/logout')
+def logout():
+    flash("Goodbye "+session['email'], 'info')
+    del session['email']
+    return redirect('/')
+
+@app.route("/")
+def index():
+    return render_template('index.html', base_title='All Blogs')
 
 @app.route('/blog', methods=['GET'])
 def blog():
@@ -40,22 +110,22 @@ def blog():
             base_title=title_header,
             header=post.title,
             body=post.body)
-
-    blogs = sorted(Blog.query.all(), key=lambda x: x.id, reverse=True)
-    return render_template('blog.html',
-        base_title=title_header,
-        header=title_header,
-        blogs=blogs)
+    else:
+        blogs = sorted(Blog.query.all(), key=lambda x: x.id, reverse=True)
+        return render_template('blog.html',
+            base_title=title_header,
+            header=title_header,
+            blogs=blogs)
 
 @app.route('/newpost', methods=['GET','POST'])
 def newpost():
     title_header = 'Add Blog Post'
     if request.method == 'POST':
-        title = request.form['title']
+        base_title = request.form['title']
         body = request.form['body']
         errors = {'title_err':"", 'body_err':""}
 
-        if title == '':
+        if base_title == '':
             errors['title_err'] = 'Please fill in Title'
         if body == '':            
             errors['body_err'] = 'Please fill in Body'
@@ -66,86 +136,28 @@ def newpost():
                 header=title_header,
                 title=title,
                 body=body,
-                title_err=errors['title_err'],
+                base_title_err=errors['title_err'],
                 body_err=errors['body_err'])
         else:
-            new_blog = Blog(title, body)
+            new_blog = Blog(base_title, body)
             db.session.add(new_blog)
             db.session.commit()
             return redirect('/blog?id={0}'.format(new_blog.id))
     else:
-        return render_template('newpost.html', base_title=title_header, header=title_header)
+        return render_template('new_post.html',
+            base_title=title_header, 
+            header=title_header)
+
+@app.route('/')
+def index():
+    return render_template('index.html', )
 
 if __name__ == "__main__":
     app.run()
 
-# @app.before_request
-# def require_login():
-#     allowed_routes = ['login', 'register']
-#     if request.endpoint not in allowed_routes and 'email' not in session:
-#         return redirect('/login')
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method== 'POST':
-#         email = request.form['email']
-#         password = request.form['password']
-#         verify = request.form['verify']
-
-#         # validate user data
-#         existing_user = User.query.filter_by(email=email).first()
-#         validation = validate_signup(email,password,verify)
-
-#         if validation == True and not existing_user:
-#             new_user = User(email, make_pw_hash(password))
-#             db.session.add(new_user)
-#             db.session.commit()
-
-#             session['email'] = email
-#             flash("Registered as " + email, 'info')
-#             return redirect('/')
-#         else:
-#             flash("Registration Failed!", 'error')
-#             return render_template('register.html',
-#                 title='Register',
-#                 email=validation['email'],
-#                 email_err=validation['email_err'],
-#                 password_err=validation['password_err'],
-#                 verify_err=validation['verify_err'])
-#     else:        
-#         return render_template('register.html', title='Register')
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         password = request.form['password']
-
-#         user = User.query.filter_by(email=email).first()
-#         validation = validate_login(user, password)
-
-#         if validation == True:
-#             session['email'] = email
-#             flash("Welcome "+email, 'info')
-#             return redirect('/')
-#         else:
-#             flash("Login Failed!", 'error')
-#             return render_template('login.html',
-#                 title='Login Error!',
-#                 email=validation['email'],
-#                 email_err=validation['email_err'],
-#                 password_err=validation['password_err'])
-#     else:
-#         return render_template('login.html', title='Login')
-
-# @app.route('/logout')
-# def logout():
-#     flash("Goodbye "+session['email'], 'info')
-#     del session['email']
-#     return redirect('/')
-
-# @app.route('/delete-task', methods=['POST'])
-# def delete_task():
+# @app.route('/delpost', methods=['POST'])
+# def delete_post():
 
 #     task_id = int(request.form['task-id'])
 #     task = Task.query.get(task_id)
