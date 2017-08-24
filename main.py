5419de55ca555c1bb999f2343ab0bb6d1bded0f2
Danny Neira <danny.neira@gmail.com>
2017-08-23 21:36:16 -0400

    More progress on login route

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class User(db.Model):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register', '/']
+    allowed_routes = ['login', 'register', ' ', 'blog']
     if request.endpoint not in allowed_routes and 'email' not in session:
         return redirect('/login')
 
